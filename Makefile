57fa1ca7c04822bac8d4980b19b405cf5923af56
Martin Czygan <martin.czygan@gmail.com>
2013-09-03 15:43:18 +0200

    make fmt; and use spaces instead of tabs

--- Makefile
+++ Makefile
@@ -1,7 +1,12 @@
 all:
 	go build -o lok2tsv lok2tsv.go
-	go build -o marcdump marcdump.go
 	go build -o marc2tsv marc2tsv.go
+	go build -o marcdump marcdump.go
 
 clean:
-	rm -f lok2tsv marcdump marc2tsv
+	rm -f lok2tsv marc2tsv marcdump
+
+fmt:
+	gofmt -w -tabs=false lok2tsv.go
+	gofmt -w -tabs=false marc2tsv.go
+	gofmt -w -tabs=false marcdump.go
--- marc2tsv.go
+++ marc2tsv.go
@@ -1,108 +1,108 @@
 package main
 
 import (
-	"./marc21" // https://gitorious.org/marc21-go/marc21
-	"flag"
-	"fmt"
-	"io"
-	"os"
-	"regexp"
-	"strings"
+        "./marc21" // https://gitorious.org/marc21-go/marc21
+        "flag"
+        "fmt"
+        "io"
+        "os"
+        "regexp"
+        "strings"
 )
 
 const version = "1.0.0"
 
 func main() {
 
-	version := flag.Bool("v", false, "prints current program version")
-	fillna := flag.String("f", "<NULL>", "fill missing values with this")
-	skip := flag.Bool("k", false, "skip lines with missing values")
+        version := flag.Bool("v", false, "prints current program version")
+        fillna := flag.String("f", "<NULL>", "fill missing values with this")
+        skip := flag.Bool("k", false, "skip lines with missing values")
 
-	var PrintUsage = func() {
-		fmt.Fprintf(os.Stderr, "Usage: %s [OPTIONS] MARCFILE TAG [TAG, ...]\n", os.Args[0])
-		flag.PrintDefaults()
-	}
+        var PrintUsage = func() {
+                fmt.Fprintf(os.Stderr, "Usage: %s [OPTIONS] MARCFILE TAG [TAG, ...]\n", os.Args[0])
+                flag.PrintDefaults()
+        }
 
-	flag.Parse()
+        flag.Parse()
 
-	if *version {
-		fmt.Println(version)
-		os.Exit(0)
-	}
+        if *version {
+                fmt.Println(version)
+                os.Exit(0)
+        }
 
-	if flag.NArg() < 2 {
-		PrintUsage()
-		os.Exit(1)
-	}
+        if flag.NArg() < 2 {
+                PrintUsage()
+                os.Exit(1)
+        }
 
-	fi, err := os.Open(flag.Args()[0])
-	if err != nil {
-		panic(err)
-	}
+        fi, err := os.Open(flag.Args()[0])
+        if err != nil {
+                panic(err)
+        }
 
-	defer func() {
-		if err := fi.Close(); err != nil {
-			panic(err)
-		}
-	}()
+        defer func() {
+                if err := fi.Close(); err != nil {
+                        panic(err)
+                }
+        }()
 
-	resf, err := regexp.Compile(`^([\d]{3})\.([a-z0-9])$`)
-	if err != nil {
-		panic("invalid regex")
-	}
-	recf, err := regexp.Compile(`^[\d]{3}$`)
-	if err != nil {
-		panic("invalid regex")
-	}
+        resf, err := regexp.Compile(`^([\d]{3})\.([a-z0-9])$`)
+        if err != nil {
+                panic("invalid regex")
+        }
+        recf, err := regexp.Compile(`^[\d]{3}$`)
+        if err != nil {
+                panic("invalid regex")
+        }
 
-	tags := flag.Args()[1:]
+        tags := flag.Args()[1:]
 
-	for {
-		record, err := marc21.ReadRecord(fi)
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			panic(err)
-		}
+        for {
+                record, err := marc21.ReadRecord(fi)
+                if err == io.EOF {
+                        break
+                }
+                if err != nil {
+                        panic(err)
+                }
 
-		var line []string
-		skipline := false
+                var line []string
+                skipline := false
 
-		for _, tag := range tags {
-			if recf.MatchString(tag) == true {
-				fields := record.GetFields(tag)
-				if len(fields) > 0 {
-					line = append(line, fields[0].(*marc21.ControlField).Data)
-				} else {
-					if *skip {
-						skipline = true
-						break
-					}
-					line = append(line, *fillna) // or any fill value
-				}
-			}
+                for _, tag := range tags {
+                        if recf.MatchString(tag) == true {
+                                fields := record.GetFields(tag)
+                                if len(fields) > 0 {
+                                        line = append(line, fields[0].(*marc21.ControlField).Data)
+                                } else {
+                                        if *skip {
+                                                skipline = true
+                                                break
+                                        }
+                                        line = append(line, *fillna) // or any fill value
+                                }
+                        }
 
-			if resf.MatchString(tag) == true {
-				parts := strings.Split(tag, ".")
-				subfields := record.GetSubFields(parts[0], []byte(parts[1])[0])
-				if len(subfields) > 0 {
-					line = append(line, subfields[0].Value)
-				} else {
-					if *skip {
-						skipline = true
-						break
-					}
-					line = append(line, *fillna) // or any fill value  
-				}
-			}
-		}
-		if skipline {
-			continue
-		} else {
-			fmt.Printf("%s\n", strings.Join(line, "\t"))
-			line = line[:0]
-		}
-	}
-	return
+                        if resf.MatchString(tag) == true {
+                                parts := strings.Split(tag, ".")
+                                subfields := record.GetSubFields(parts[0], []byte(parts[1])[0])
+                                if len(subfields) > 0 {
+                                        line = append(line, subfields[0].Value)
+                                } else {
+                                        if *skip {
+                                                skipline = true
+                                                break
+                                        }
+                                        line = append(line, *fillna) // or any fill value  
+                                }
+                        }
+                }
+                if skipline {
+                        continue
+                } else {
+                        fmt.Printf("%s\n", strings.Join(line, "\t"))
+                        line = line[:0]
+                }
+        }
+        return
 }
