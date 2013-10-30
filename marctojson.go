8c22cf36408a743bc221a52ceb42e96ff96dc6cc
Martin Czygan <martin.czygan@gmail.com>
2013-10-30 11:26:32 +0100

    first functional version of marctojson.go

--- marctojson.go
+++ marctojson.go
@@ -6,22 +6,35 @@ import (
     "fmt"
     "io"
     "os"
-    "regexp"
     "strings"
     "encoding/json"
 )
 
 const app_version = "1.0.0"
 
+func stringToMap(s string) map[string]string {
+    result := make(map[string]string)
+    for _, pair := range strings.Split(s, ",") {
+        fmt.Println(pair)
+        kv := strings.Split(pair, "=")
+        if len(kv) != 2 {
+            panic(fmt.Sprintf("Could not parse key-value parameter: %s", s))
+        } else {
+            result[kv[0]] = kv[1]
+        }
+    }
+    return result
+}
+
 func main() {
 
-    // fillna := flag.String("f", "<NULL>", "fill missing values with this")
     ignore := flag.Bool("i", false, "ignore marc errors (not recommended)")
-    skip := flag.Bool("k", false, "skip lines with missing values")
     version := flag.Bool("v", false, "prints current program version")
 
+    var meta *string = flag.String("m", "", "a key=value pair to pass to meta")
+
     var PrintUsage = func() {
-        fmt.Fprintf(os.Stderr, "Usage: %s [OPTIONS] MARCFILE TAG [TAG, ...]\n", os.Args[0])
+        fmt.Fprintf(os.Stderr, "Usage: %s [OPTIONS] MARCFILE\n", os.Args[0])
         flag.PrintDefaults()
     }
 
@@ -32,7 +45,7 @@ func main() {
         os.Exit(0)
     }
 
-    if flag.NArg() < 2 {
+    if flag.NArg() < 1 {
         PrintUsage()
         os.Exit(1)
     }
@@ -48,19 +61,7 @@ func main() {
         }
     }()
 
-    // regex subfield
-    resf, err := regexp.Compile(`^([\d]{3})\.([a-z0-9])$`)
-    if err != nil {
-        panic("invalid regex")
-    }
-
-    // regex controlfield
-    recf, err := regexp.Compile(`^[\d]{3}$`)
-    if err != nil {
-        panic("invalid regex")
-    }
-
-    tags := flag.Args()[1:]
+    metamap := stringToMap(*meta)
 
     for {
         record, err := marc21.ReadRecord(fi)
@@ -76,83 +77,55 @@ func main() {
             }
         }
 
-        // var line []string
-        skipline := false
+        mainmap := make(map[string]interface{})
 
-        marcmap := make(map[string]string)
+        marcmap := make(map[string]interface{})
+        leadermap := make(map[string]string)
 
-        for _, tag := range tags {
+        leader := record.Leader
+        leadermap["status"] = string(leader.Status)
+        leadermap["cs"] = string(leader.CharacterEncoding)
+        leadermap["length"] = fmt.Sprintf("%d", leader.Length)
+        leadermap["type"] = string(leader.Type)
+        leadermap["impldef"] = string(leader.ImplementationDefined[:5])
+        leadermap["ic"] = fmt.Sprintf("%d", leader.IndicatorCount)
+        leadermap["lol"] = fmt.Sprintf("%d", leader.LengthOfLength)
+        leadermap["losp"] = fmt.Sprintf("%d", leader.LengthOfStartPos)
+        leadermap["sfcl"] = fmt.Sprintf("%d", leader.SubfieldCodeLength)
+        leadermap["ba"] = fmt.Sprintf("%d", leader.BaseAddress)
+        leadermap["raw"] = string(leader.Bytes())
 
-            if recf.MatchString(tag) {
-                fields := record.GetFields(tag)
-                if len(fields) > 0 {
-                    // line = append(line, fields[0].(*marc21.ControlField).Data)
-                    marcmap[tag] = fields[0].(*marc21.ControlField).Data;
-                } else {
-                    if *skip {
-                        skipline = true
-                        break
-                    }
-                    // line = append(line, *fillna) // or any fill value
+        marcmap["leader"] = leadermap
+
+        for _, field := range record.Fields {
+            tag := field.GetTag()
+            if strings.HasPrefix(tag, "00") {
+                marcmap[tag] = field.(*marc21.ControlField).Data
+            } else {
+                datafield := field.(*marc21.DataField)
+                subfieldmap := make(map[string]string)
+                for _, subfield := range datafield.SubFields {
+                    subfieldmap[fmt.Sprintf("%c", subfield.Code)] = subfield.Value
                 }
-            } else if resf.MatchString(tag) {
-                parts := strings.Split(tag, ".")
-                subfields := record.GetSubFields(parts[0], []byte(parts[1])[0])
-                if len(subfields) > 0 {
-                    // line = append(line, subfields[0].Value)
-                    marcmap[tag] = subfields[0].Value;
-                } else {
-                    if *skip {
-                        skipline = true
-                        break
-                    }
-                    // line = append(line, *fillna) // or any fill value
+                _, present := marcmap[tag]
+                if !present {
+                    subfields := make([]interface{}, 0)
+                    marcmap[tag] = subfields
                 }
-            } else if strings.HasPrefix(tag, "@") {
-                // leader := record.Leader
-                // switch tag {
-                // case "@Length":
-                //     line = append(line, fmt.Sprintf("%d", leader.Length))
-                // case "@Status":
-                //     line = append(line, string(leader.Status))
-                // case "@Type":
-                //     line = append(line, string(leader.Type))
-                // case "@ImplementationDefined":
-                //     line = append(line, string(leader.ImplementationDefined[:5]))
-                // case "@CharacterEncoding":
-                //     line = append(line, string(leader.CharacterEncoding))
-                // case "@BaseAddress":
-                //     line = append(line, fmt.Sprintf("%d", leader.BaseAddress))
-                // case "@IndicatorCount":
-                //     line = append(line, fmt.Sprintf("%d", leader.IndicatorCount))
-                // case "@SubfieldCodeLength":
-                //     line = append(line, fmt.Sprintf("%d", leader.SubfieldCodeLength))
-                // case "@LengthOfLength":
-                //     line = append(line, fmt.Sprintf("%d", leader.LengthOfLength))
-                // case "@LengthOfStartPos":
-                //     line = append(line, fmt.Sprintf("%d", leader.LengthOfStartPos))
-                // default:
-                //     panic(fmt.Sprintf("tag not recognized: %s (see: https://github.com/miku/gomarckit)", tag))
-                //}
-            } else if !strings.HasPrefix(tag, "-") {
-                // line = append(line, strings.TrimSpace(tag))
+                marcmap[tag] = append(marcmap[tag].([]interface{}), subfieldmap)
             }
         }
 
-        if skipline {
-            continue
-        } else {
-            // fmt.Printf("%s\n", strings.Join(line, "\t"))
-            // line = line[:0]
+        mainmap["content"] = marcmap
+        mainmap["meta"] = metamap
+        mainmap["content_type"] = "application/marc"
 
-            b, err := json.Marshal(marcmap)
-            if err != nil {
-                fmt.Println("error:", err)
-            }
-            os.Stdout.Write(b)
-            fmt.Println()
+        b, err := json.Marshal(mainmap)
+        if err != nil {
+            fmt.Println("error:", err)
         }
+        os.Stdout.Write(b)
+        fmt.Println()
 
     }
-    return
 }
