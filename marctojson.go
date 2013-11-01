130c3e46dab92680b036281e0746582264e8cec4
Martin Czygan <martin.czygan@gmail.com>
2013-11-01 18:06:51 +0100

    fixed shadowed repeated subfield code bug

--- marctojson.go
+++ marctojson.go
@@ -1,7 +1,41 @@
+// Turn MARC into JSON.
+// The JSON format was choosen with Elasticsearch queries in mind. Also
+// the format proposed [in this blog post](http://dilettantes.code4lib.org/blog/2010/09/a-proposal-to-serialize-marc-in-json/)
+// had some verbose elements, such as an extra subfields key.
+//
+// Java version (without original and sha1)
+//
+// $ time target/marctojson -i ../gomarckit/test-tit.mrc -o ../gomarckit/test-tit.json.3
+//
+// real    25m34.243s
+// user    24m13.468s
+// sys      0m17.348s
+//
+// $ time ./marctojson test-tit.mrc > test-tit.json.1
+//
+// real    12m32.742s
+// user     9m52.720s
+// sys      1m47.288s
+//
+// $ time ./marctojson.py MarcToJSONMerged --filename test-tit.mrc --workers 4
+// DEBUG: [SplitMarc.run] 51.48869
+// DEBUG: [MarcToJSON.run] 209.79476
+// DEBUG: [MarcToJSON.run] 1.84064
+// DEBUG: [MarcToJSON.run] 229.05073
+// DEBUG: [MarcToJSON.run] 260.67292
+// DEBUG: [MarcToJSON.run] 261.16039
+// DEBUG: [MarcToJSONMerged.run] 170.03256
+
+// real     8m3.518s
+// user    10m0.568s
+// sys      2m17.676s
+
+//
+// Based on https://gitorious.org/marc21-go/marc21, which is included here.
 package main
 
 import (
-    "./marc21" // https://gitorious.org/marc21-go/marc21
+    "./marc21"
     "encoding/json"
     "flag"
     "fmt"
@@ -10,8 +44,9 @@ import (
     "strings"
 )
 
-const app_version = "1.0.0"
+const app_version = "1.0.1"
 
+// Turn a list of key=value,key=value strings into a map.
 func stringToMap(s string) map[string]string {
     result := make(map[string]string)
     if len(s) == 0 {
@@ -34,7 +69,7 @@ func main() {
     version := flag.Bool("v", false, "prints current program version")
 
     metaVar := flag.String("m", "", "a key=value pair to pass to meta")
-    filterVar := flag.String("r", "", "only dump the given tags (comma separated list)")
+    filterVar := flag.String("r", "", "only dump the given tags (e.g. 001,003)")
 
     var PrintUsage = func() {
         fmt.Fprintf(os.Stderr, "Usage: %s [OPTIONS] MARCFILE\n", os.Args[0])
@@ -88,9 +123,11 @@ func main() {
             }
         }
 
+        // the final map
         mainMap := make(map[string]interface{})
-
+        // content map
         marcMap := make(map[string]interface{})
+        // the expanded leader
         leaderMap := make(map[string]string)
 
         leader := record.Leader
@@ -120,9 +157,27 @@ func main() {
                 marcMap[tag] = field.(*marc21.ControlField).Data
             } else {
                 datafield := field.(*marc21.DataField)
-                subfieldMap := make(map[string]string)
+                subfieldMap := make(map[string]interface{})
+                subfieldMap["ind1"] = string(datafield.Ind1)
+                subfieldMap["ind2"] = string(datafield.Ind2)
                 for _, subfield := range datafield.SubFields {
-                    subfieldMap[fmt.Sprintf("%c", subfield.Code)] = subfield.Value
+                    code := fmt.Sprintf("%c", subfield.Code)
+                    value, present := subfieldMap[code]
+                    if present {
+                        switch t := value.(type) {
+                        default:
+                            panic(fmt.Sprintf("unexpected type: %T", t))
+                        case string:
+                            values := make([]string, 0)
+                            values = append(values, value.(string))
+                            values = append(values, subfield.Value)
+                            subfieldMap[code] = values
+                        case []string:
+                            subfieldMap[code] = append(subfieldMap[code].([]interface{}), subfield.Value)
+                        }
+                    } else {
+                        subfieldMap[code] = subfield.Value
+                    }
                 }
                 _, present := marcMap[tag]
                 if !present {
@@ -143,6 +198,5 @@ func main() {
         }
         os.Stdout.Write(b)
         fmt.Println()
-
     }
 }
