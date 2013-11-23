1196a28fdbe01b59b786fdf1bdf84e5b2ff27a95
Martin Czygan <martin.czygan@gmail.com>
2013-11-23 19:21:17 +0100

    first version of marcxmltojson

--- marcxmltojson.go
+++ marcxmltojson.go
@@ -1,12 +1,42 @@
-// WIP, prototype
 // marcxml to es flavoured json
+// Example XML
+
+// <?xml version="1.0" encoding="utf-8"?>
+// <marc:collection xmlns:marc="http://www.loc.gov/MARC21/slim">
+// <marc:record xmlns:marc="http://www.loc.gov/MARC21/slim">
+// <marc:leader>     njm a22     2u 4500</marc:leader>
+// <marc:controlfield tag="001">NML00000001</marc:controlfield>
+// <marc:controlfield tag="003">DE-Khm1</marc:controlfield>
+// <marc:controlfield tag="005">20130916115438</marc:controlfield>
+// <marc:controlfield tag="006">m||||||||h||||||||</marc:controlfield>
+// <marc:controlfield tag="007">cr nnannnu uuu</marc:controlfield>
+// <marc:controlfield tag="008">130916s2013</marc:controlfield>
+// <marc:datafield tag="028" ind1="1" ind2="1">
+// <marc:subfield code="a">8.220369</marc:subfield>
+// <marc:subfield code="b">Naxos Digital Services Ltd</marc:subfield>
+// </marc:datafield>
+// <marc:datafield tag="035" ind1=" " ind2=" ">
+// <marc:subfield code="a">(DE-Khm1)NML00000001</marc:subfield>
+// </marc:datafield>
+// <marc:datafield tag="040" ind1=" " ind2=" ">
+// <marc:subfield code="a">DE-Khm1</marc:subfield>
+// <marc:subfield code="b">ger</marc:subfield>
+// <marc:subfield code="c">DE-Khm1</marc:subfield>
+// </marc:datafield>
+// </marc:record>
+// ...
+// </marc:collection>
+
 package main
 
 import (
     "encoding/json"
     "encoding/xml"
+    "flag"
     "fmt"
+    "io/ioutil"
     "os"
+    "strings"
 )
 
 const app_version = "1.0"
@@ -34,129 +64,120 @@ type Record struct {
     DataFields    []DataField    `xml:"http://www.loc.gov/MARC21/slim datafield"`
 }
 
-func (record *Record) AsMap() map[string]interface{} {
+type Collection struct {
+    XMLName xml.Name `xml:"http://www.loc.gov/MARC21/slim collection"`
+    Records []Record `xml:"http://www.loc.gov/MARC21/slim record"`
+}
+
+// Turn a list of key=value,key=value strings into a map.
+func stringToMap(s string) map[string]string {
+    result := make(map[string]string)
+    if len(s) == 0 {
+        return result
+    }
+    for _, pair := range strings.Split(s, ",") {
+        kv := strings.Split(pair, "=")
+        if len(kv) != 2 {
+            panic(fmt.Sprintf("Could not parse key-value parameter: %s", s))
+        } else {
+            result[kv[0]] = kv[1]
+        }
+    }
+    return result
+}
+
+// Convert a single unmarshalled record struct into a map.
+func (record *Record) ToMap() map[string]interface{} {
     dict := make(map[string]interface{})
     dict["leader"] = record.Leader
     for _, field := range record.ControlFields {
         dict[field.Tag] = field.Value
     }
     for _, field := range record.DataFields {
-        submap := make(map[string]string)
+        submap := make(map[string]interface{})
+        submap["ind1"] = field.Ind1
+        submap["ind2"] = field.Ind2
         for _, subfield := range field.Subfields {
-            submap[subfield.Code] = subfield.Value
+            value, present := submap[subfield.Code]
+            if present {
+                switch t := value.(type) {
+                default:
+                    panic(fmt.Sprintf("unexpected type: %T", t))
+                case string:
+                    values := make([]string, 0)
+                    values = append(values, value.(string))
+                    values = append(values, subfield.Value)
+                    submap[subfield.Code] = values
+                case []string:
+                    submap[subfield.Code] = append(submap[subfield.Code].([]string), subfield.Value)
+                }
+            } else {
+                submap[subfield.Code] = subfield.Value
+            }
         }
-        dict[field.Tag] = submap
+        _, present := dict[field.Tag]
+        if !present {
+            subfields := make([]interface{}, 0)
+            dict[field.Tag] = subfields
+        }
+        dict[field.Tag] = append(dict[field.Tag].([]interface{}), submap)
     }
     return dict
 }
 
 func main() {
 
-    data := `
-<marc:record xmlns:marc="http://www.loc.gov/MARC21/slim">
-<marc:leader>     njm a22     2u 4500</marc:leader>
-<marc:controlfield tag="001">NML00000001</marc:controlfield>
-<marc:controlfield tag="003">DE-Khm1</marc:controlfield>
-<marc:controlfield tag="005">20130916115438</marc:controlfield>
-<marc:controlfield tag="006">m||||||||h||||||||</marc:controlfield>
-<marc:controlfield tag="007">cr nnannnu uuu</marc:controlfield>
-<marc:controlfield tag="008">130916s2013</marc:controlfield>
-<marc:datafield tag="028" ind1="1" ind2="1">
-<marc:subfield code="a">8.220369</marc:subfield>
-<marc:subfield code="b">Naxos Digital Services Ltd</marc:subfield>
-</marc:datafield>
-<marc:datafield tag="035" ind1=" " ind2=" ">
-<marc:subfield code="a">(DE-Khm1)NML00000001</marc:subfield>
-</marc:datafield>
-<marc:datafield tag="040" ind1=" " ind2=" ">
-<marc:subfield code="a">DE-Khm1</marc:subfield>
-<marc:subfield code="b">ger</marc:subfield>
-<marc:subfield code="c">DE-Khm1</marc:subfield>
-</marc:datafield>
-<marc:datafield tag="041" ind1=" " ind2=" ">
-<marc:subfield code="a">ger</marc:subfield>
-</marc:datafield>
-<marc:datafield tag="100" ind1="1" ind2=" ">
-<marc:subfield code="a">Ippolitov-Ivanov, Mikhail Mikhaylovich</marc:subfield>
-<marc:subfield code="d">1859-1935</marc:subfield>
-</marc:datafield>
-<marc:datafield tag="245" ind1="0" ind2="0">
-<marc:subfield code="a">IPPOLITOV-IVANOV : Caucasian Sketches</marc:subfield>
-<marc:subfield code="h">[elektronische Ressource]</marc:subfield>
-</marc:datafield>
-<marc:datafield tag="260" ind1=" " ind2=" ">
-<marc:subfield code="b">Marco Polo</marc:subfield>
-</marc:datafield>
-<marc:datafield tag="500" ind1=" " ind2=" ">
-<marc:subfield code="a">Streaming audio.</marc:subfield>
-</marc:datafield>
-<marc:datafield tag="505" ind1="0" ind2="0">
-<marc:subfield code="t">Caucasian Sketches, Suite 1, Op. 10: In a Mountain Pass / In the Village / In the Mosque / Procession of the Sardar / Ippolitov-Ivanov, Mikhail Mikhaylovich</marc:subfield>
-<marc:subfield code="g">00:25:11 --</marc:subfield>
-<marc:subfield code="t">Caucasian Sketches, Suite 2, Op. 42, "Iveria": Introduction / Berceuse / Lesghinka / Caucasian War March / Ippolitov-Ivanov, Mikhail Mikhaylovich</marc:subfield>
-<marc:subfield code="g">00:23:33 --</marc:subfield>
-</marc:datafield>
-<marc:datafield tag="506" ind1=" " ind2=" ">
-<marc:subfield code="a">Der Zugang ist auf registrierte Benutzer beschränkt.</marc:subfield>
-</marc:datafield>
-<marc:datafield tag="511" ind1="0" ind2=" ">
-<marc:subfield code="a">Ippolitov-Ivanov, Mikhail Mikhaylovich, Komponist -- Lyndon-Gee, Christopher, Dirigent -- Sydney Symphony Orchestra, Orchester</marc:subfield>
-</marc:datafield>
-<marc:datafield tag="538" ind1=" " ind2=" ">
-<marc:subfield code="a">Systemvoraussetzungen (minimale Voraussetzungen): MS Windows 98SE, 2000, XP with MS IE 6.0 / Mozilla 1.7.1 / FireFox 1PR / Netscape 7.1 / Opera 7.53 and Media Player 9.0 / 10.0.</marc:subfield>
-</marc:datafield>
-<marc:datafield tag="648" ind1=" " ind2="4">
-<marc:subfield code="y">20. Jahrhundert</marc:subfield>
-</marc:datafield>
-<marc:datafield tag="650" ind1=" " ind2="0">
-<marc:subfield code="a">Musik</marc:subfield>
-<marc:subfield code="x">Computer- und Netzwerkressourcen.</marc:subfield>
-</marc:datafield>
-<marc:datafield tag="655" ind1=" " ind2="4">
-<marc:subfield code="a">Orchestermusik</marc:subfield>
-</marc:datafield>
-<marc:datafield tag="700" ind1="1" ind2=" ">
-<marc:subfield code="a">Ippolitov-Ivanov, Mikhail Mikhaylovich</marc:subfield>
-<marc:subfield code="c">Komponist</marc:subfield>
-<marc:subfield code="d">1859-1935</marc:subfield>
-</marc:datafield>
-<marc:datafield tag="710" ind1="2" ind2=" ">
-<marc:subfield code="a">Sydney Symphony Orchestra</marc:subfield>
-</marc:datafield>
-<marc:datafield tag="773" ind1="0" ind2=" ">
-<marc:subfield code="t">Naxos Music Library.</marc:subfield>
-</marc:datafield>
-<marc:datafield tag="776" ind1="0" ind2=" ">
-<marc:subfield code="d">Hong Kong, Naxos Digital Services Ltd.</marc:subfield>
-</marc:datafield>
-<marc:datafield tag="830" ind1=" " ind2="0">
-<marc:subfield code="a"/>
-<marc:subfield code="p">Caucasian Sketches, Suite 1, Op. 10 -- Caucasian Sketches, Suite 2, Op. 42, "Iveria"</marc:subfield>
-</marc:datafield>
-<marc:datafield tag="856" ind1="4" ind2="0">
-<marc:subfield code="u">http://univportal.naxosmusiclibrary.com/catalogue/item.asp?cid=8.220369</marc:subfield>
-<marc:subfield code="z">Verbindung zu Naxos Music Library</marc:subfield>
-</marc:datafield>
-<marc:datafield tag="902" ind1=" " ind2=" ">
-<marc:subfield code="a">130916</marc:subfield>
-</marc:datafield>
-</marc:record>    
-    `
+    plainVar := flag.Bool("p", false, "plain mode: dump without content and meta")
+    metaVar := flag.String("m", "", "a key=value pair to pass to meta")
 
-    v := Record{}
-    err := xml.Unmarshal([]byte(data), &v)
-    if err != nil {
-        fmt.Printf("%v", err)
+    var PrintUsage = func() {
+        fmt.Fprintf(os.Stderr, "Usage: %s MARCFILE\n", os.Args[0])
+        flag.PrintDefaults()
+    }
+
+    flag.Parse()
+
+    if flag.NArg() < 1 {
+        PrintUsage()
         os.Exit(1)
-    } else {
-        // fmt.Println(v.AsMap())
-        // fmt.Printf("%v", v)
-        b, err := json.Marshal(v.AsMap())
+    }
+
+    handle, err := os.Open(flag.Args()[0])
+    if err != nil {
+        fmt.Println("Error opening file:", err)
+        os.Exit(1)
+    }
+
+    defer func() {
+        if err := handle.Close(); err != nil {
+            panic(err)
+        }
+    }()
+
+    b, err := ioutil.ReadAll(handle)
+    if err != nil {
+        fmt.Println("Error reading file:", err)
+        os.Exit(1)
+    }
+
+    metamap := stringToMap(*metaVar)
+
+    collection := Collection{}
+    err = xml.Unmarshal(b, &collection)
+
+    for _, record := range collection.Records {
+        recordMap := record.ToMap()
+        result := recordMap
+        if !*plainVar {
+            result = make(map[string]interface{})
+            result["content"] = recordMap
+            result["meta"] = metamap
+        }
+        b, err = json.Marshal(result)
         if err != nil {
-            panic(fmt.Sprintf("error: %s", err))
+            panic(fmt.Sprintf("Conversion error: %s", err))
         }
         os.Stdout.Write(b)
         fmt.Println()
-
     }
 }
